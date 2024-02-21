709e0b4a9500441f5d90e4f3a68d2c0de986c76c
Kiky <rizkyramdani1402@gmai.com>
2024-02-22 00:05:43 +0700

    menambahkan timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,37 +1,51 @@
-import React from 'react';
+import React from "react";
 
 const experiences = [
   {
     id: 1,
-    role: 'Freelance Developer',
-    company: 'Spill Notify',
-    date: 'No Specific Date',
-    description: 'Developed a tool that allows users of a group to buy limited time items faster than others by monitoring the websites using HTTP requests. More info here.',
+    role: "Freelance Developer",
+    company: "Spill Notify",
+    date: "No Specific Date",
+    description: "Developed a tool that allows users of a group to buy limited time items faster than others by monitoring the websites using HTTP requests. More info here.",
   },
   {
     id: 2,
-    role: 'Software Developer',
-    company: 'Ambush.IO',
-    date: 'Aug 2021 - Apr 2022',
-    description: 'Used Python + Flask to develop the software for a SaaS with a team of developers. More info here.',
+    role: "Software Developer",
+    company: "Ambush.IO",
+    date: "Aug 2021 - Apr 2022",
+    description: "Used Python + Flask to develop the software for a SaaS with a team of developers. More info here.",
   },
   {
     id: 3,
-    role: 'Developer/Owner',
-    company: 'LiquidTools',
-    date: 'Apr 2022 - Current',
-    description: 'A SaaS start up I developed in my spare time that now has 200+ monthly paying users. Used Python + JS. More info here.',
+    role: "Developer/Owner",
+    company: "LiquidTools",
+    date: "Apr 2022 - Current",
+    description: "A SaaS start up I developed in my spare time that now has 200+ monthly paying users. Used Python + JS. More info here.",
+  },
+  {
+    id: 4,
+    role: "GIS Analyst",
+    company: "LiquidTools",
+    date: "Apr 2022 - Current",
+    description: "A SaaS start up I developed in my spare time that now has 200+ monthly paying users. Used Python + JS. More info here.",
+  },
+  {
+    id: 5,
+    role: "GIS DEVELOPER",
+    company: "LiquidTools",
+    date: "Apr 2022 - Current",
+    description: "A SaaS start up I developed in my spare time that now has 200+ monthly paying users. Used Python + JS. More info here.",
   },
 ];
 
 interface TimelineItemProps {
-  experience: typeof experiences[0];
-  side: 'left' | 'right';
+  experience: (typeof experiences)[0];
+  side: "left" | "right";
 }
 
 const TimelineItem: React.FC<TimelineItemProps> = ({ experience, side }) => {
   return (
-    <div className={`relative w-full my-6 ${side === 'right' ? 'md:ml-auto' : 'md:mr-auto'} md:w-2/5 ${side === 'right' ? 'timeline-bg1' : 'timeline-bg2'}`}>
+    <div className={`relative w-full my-6 ${side === "right" ? "md:ml-auto" : "md:mr-auto"} md:w-2/5 ${side === "right" ? "timeline-bg1" : "timeline-bg2"}`}>
       <div className={`bg-transparent bg-transparent border border-grey rounded-lg p-6 `}>
         <h3 className="text-xl font-semibold mb-2">{experience.role}</h3>
         <h4 className="text-purple-300 text-purple-300 mb-4">{experience.company}</h4>
@@ -50,11 +64,7 @@ const VerticalTimeline: React.FC = () => {
         <div className="relative w-full">
           <div className="hidden md:block border-l-2 border-purple-600 border-purple-450 absolute left-1/2 transform -translate-x-1/2 h-full"></div>
           {experiences.map((experience, index) => (
-            <TimelineItem
-              key={experience.id}
-              experience={experience}
-              side={index % 2 === 0 ? 'left' : 'right'}
-            />
+            <TimelineItem key={experience.id} experience={experience} side={index % 2 === 0 ? "left" : "right"} />
           ))}
         </div>
       </div>
